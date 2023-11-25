4a390d021ed6dfbb4d2722eebccbe9a3a7f36bb6
Randy Selner <e_u727@hotmail.com>
2023-11-24 16:51:09 -0800

    shortened code length, changed name.

--- Project2/Source.cpp
+++ Project2/Source.cpp
@@ -1,49 +1,36 @@
-//Project2.cpp
+//Kaprekation_Game.cpp
 //This project is the kaprekar function brought to all bases, displaying the k-number and loop length number as k.l for the table.
 #include <iostream>
 #include <cmath>
 using namespace std;
-
 struct container
 {
 	bool not_same = true;
 	double value = 0;
+	double base_value = 0;
 	int counter = 0;
 	int number_of_times_played = 0;
 	int game_number = 0;
-	double base_value = 0;
 	int whole_base = 0;
 	int whole_number = 0;
 	int loop_length = 0;
 }c;
-
 void get_input_pt_1(container* c);
 void game_2(container* c);
 void game_1(container* c);
 void sort_array(int digits[]);
 double kaprekate(container* c, int array[]);
-
 int main()
-{
-	char response = 'o';
+{ char response = 'o';
 	while (response != 'n')
 	{
 		cout << "Want to play ";
-		if (c.number_of_times_played == 0)
-		{
-			cout << "a game?\n\ny/n: ";
-			cin >> response;
-		}
-		else
-		{
-			cout << "again?\n\ny/n: ";
-			cin >> response;
-		}
+		if (c.number_of_times_played == 0) { cout << "a game?\n\ny/n: "; cin >> response;}
+		else { cout << "again?\n\ny/n: "; cin >> response;}
 		if (response != 'n')
 		{
 			if (response == 'y')
-			{
-				get_input_pt_1(&c);
+			{ get_input_pt_1(&c);
 				if (c.game_number == 1)
 				{
 					cout << "\n\nYou chose single number style!\n";
@@ -57,35 +44,22 @@ int main()
 					cout << endl << endl;
 					c.number_of_times_played++;
 				}
-				else if (c.game_number == 0)
-				{
-					response = 'n';
+				else if (c.game_number == 0) { response = 'n';}
 				}
+			else { cout << "\n\nNot a choice, just pick y or n...\n" << endl;}
 			}
-			else
-			{
-				cout << "\n\nNot a choice, just pick y or n...\n" << endl;
-			}
-		}
 		if (response == 'n')
 		{
 			cout << "\nPlayed " << c.number_of_times_played;
-			if (c.number_of_times_played == 0 || c.number_of_times_played > 1)
-			{
-				cout << " times.";
-			}
-			else if (c.number_of_times_played == 1)
-			{
-				cout << " time.";
-			}
+			if (c.number_of_times_played == 0 || c.number_of_times_played > 1) { cout << " times.";}
+			else if (c.number_of_times_played == 1) { cout << " time.";}
 			cout << "\n\n\n~Bye!~\n" << endl;
 		}
 	}
-	return 0;
+	return 6174;
 }
 void get_input_pt_1(container* c)
-{
-	bool flag_repeat = true;
+{ bool flag_repeat = true;
 	while (flag_repeat == true)
 	{
 		cout << "\nChoose to play either single number (1) or base chart (2) style, or 0 to quit: ";
@@ -94,15 +68,11 @@ void get_input_pt_1(container* c)
 		{
 			flag_repeat = false;
 		}
-		else
-		{
-			cout << "\n\nI'm afraid that just isn't an option.. try again.\n";
+		else {cout << "\n\nI'm afraid that just isn't an option.. try again.\n";}
 		}
 	}
-}
 void game_2(container* c)
-{
-	int array[32] = {};
+{ int array[32] = {};
 	cout << "\nChoose the positive whole-number base value that you want to view the chart for\n";
 	cout << "Note: Larger bases have longer charts, so after a certain base number the beginning will be lost after the program ends.\n\nbase: ";
 	cin >> c->base_value;
@@ -129,11 +99,11 @@ void game_2(container* c)
 	}
 }
 void game_1(container* c)
-{
+{ int array[32] = {};
 	cout << "\nBegin by choosing a positive whole-number base value: ";
 	cin >> c->base_value;
 	c->whole_base = floor(c->base_value);
-	cout << "\n\nNow enter the number you wish to analyze: ";
+	cout << "\n\nNow enter the number you wish to analyze (less than " << pow(c->base_value, 4) << ") : ";
 	cin >> c->value;
 	c->whole_number = floor(c->value);
 	if (c->whole_number >= pow(c->base_value, 4))
@@ -146,57 +116,42 @@ void game_1(container* c)
 	}
 	else
 	{
-		int array[32] = {};
 		cout << "\n\nk = " << kaprekate(c, array) << "\n\n";
-		c->number_of_times_played++;
-		c->counter = 0;
-		c->not_same = true;
+		c->number_of_times_played++; c->counter = 0; c->not_same = true;
 	}
 }
 void sort_array(int digits[])
-{
-	int temp_var = 0; int smallest_value = 0;
+{ int temp_var = 0; int smallest_value = 0;
 	for (int i = 0; i < 4; i++)
-	{
-		int smallest_index = i;
+	{ int smallest_index = i;
 		for (int j = i + 1; j < 4; j++)
 		{
-			if (digits[j] < digits[smallest_index])
-			{
-				smallest_index = j;
+			if (digits[j] < digits[smallest_index]) { smallest_index = j;}
 			}
-		}
 		if (smallest_index != i)
-		{
-			temp_var = digits[i];
+		{ temp_var = digits[i];
 			digits[i] = digits[smallest_index];
 			digits[smallest_index] = temp_var;
 		}
 	}
 }
 double kaprekate(container* c, int array[])
-{
-	int digits[4] = {0, 0, 0, 0};
+{ int digits[4] = {0, 0, 0, 0};
 	for (int i = 3; i > -1; i--)
 	{
 		digits[i] = c->whole_number % c->whole_base;
 		c->whole_number -= digits[i];
 		c->whole_number /= c->whole_base;
 	}
-	if (digits[0] == digits[1] && digits[1] == digits[2] && digits[2] == digits[3])
-	{
-		c->loop_length = 0;
-		return 0;
-	}
+	if (digits[0] == digits[1] && digits[1] == digits[2] && digits[2] == digits[3]) { c->loop_length = 0; return 0;}
 	else
-	{
-		sort_array(digits);
+	{ sort_array(digits);
 		if (c->game_number == 1)
 		{
 			cout << "\narray[" << c->counter << "] = ";
-			for (int i = 0; i < 4; i++)
+			for (int j = 0; j < 4; j++)
 			{
-				cout << digits[i] << " ";
+				cout << digits[j] << " ";
 			}
 			cout << "\n";
 		}
@@ -213,9 +168,9 @@ double kaprekate(container* c, int array[])
 		if (c->game_number == 1)
 		{
 			cout << "\nnew_array[" << c->counter << "] = ";
-			for (int j = 0; j < 4; j++)
+			for (int l = 0; l < 4; l++)
 			{
-				cout << digits[j] << " ";
+				cout << digits[l] << " ";
 			}
 			cout << "\n";
 		}
@@ -225,26 +180,11 @@ double kaprekate(container* c, int array[])
 			if (array[l] == smallest_value)
 			{
 				c->loop_length = c->counter - l;
-				if (c->game_number == 1)
-				{
-					cout << "\n\nl = " << c->loop_length << "\n\n";
-				}
+				if (c->game_number == 1) { cout << "\n\nl = " << c->loop_length << "\n\n";}
 				c->not_same = false;
 			}
 		}
-		if (c->not_same == false)
-		{
-			if (c->counter == 0)
-			{
-				c->loop_length++;
-			}
-			return c->counter;
-		}
-		else if (c->not_same == true)
-		{
-			c->whole_number = smallest_value;
-			c->counter++;
-			return kaprekate(c, array);
-		}
+		if (c->not_same == false) { if (c->counter == 0) { c->loop_length++;} return c->counter;}
+		else if (c->not_same == true) { c->whole_number = smallest_value; c->counter++; return kaprekate(c, array);}
 	}
 }
